8fa5cb3a428d090fa8660a6ad8af8dccaad0df00
kshitijvarshne1 <kshitijvarshney92@gmail.com>
2026-06-20 14:59:21 +0530

    done with debounce

--- src/debounce/debounceWithoutUseEffect.jsx
+++ src/debounce/debounceWithoutUseEffect.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+
+function debounce(fn, delay) {
+  let timer;
+  return function (...args) {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn(...args);
+    }, delay);
+  };
+}
 
 function DebounceWithoutUseEffect() {
-  return <div>debounceWithoutUseEffect</div>;
+  const [text, setText] = useState("");
+  const [debouncedText, setDebouncedText] = useState("");
+
+  // create debounced function once
+  //   const handleDebouncedChange = debounce((value) => {
+  //     console.log("debounced:", value);
+  //     setDebouncedText(value);
+  //   }, 2000);
+
+  const handleDebouncedChange = useMemo(
+    () =>
+      debounce((value) => {
+        console.log("debounced:", value);
+        setDebouncedText(value);
+      }, 2000),
+    [],
+  );
+
+  function handleChange(e) {
+    const value = e.target.value;
+    setText(value);
+
+    handleDebouncedChange(value);
+  }
+
+  return (
+    <div>
+      <input value={text} onChange={handleChange} />
+      <p>
+        <strong>Real-time:</strong> {text}
+      </p>
+      <p>
+        <strong>Debounced:</strong> {debouncedText}
+      </p>
+    </div>
+  );
 }
 
 export default DebounceWithoutUseEffect;

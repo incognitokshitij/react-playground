8427f7badfb588f612d20f7538d64218f0dad421
kshitijvarshne1 <kshitijvarshney92@gmail.com>
2026-06-20 14:44:41 +0530

    debounce with use effect

--- src/debounce/index.jsx
+++ src/debounce/index.jsx
@@ -1,5 +1,35 @@
+import { useState, useEffect } from "react";
+
 function DebounceWithUseEffect() {
-  return <div>Debounce</div>;
+  const [text, setText] = useState("");
+  const [debounceText, setDebounceText] = useState("");
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      console.log("setting debounce value", text);
+      setDebounceText(text);
+    }, 500);
+
+    return () => {
+      console.log("clear timeout");
+      clearInterval(timer);
+    };
+  }, [text]);
+
+  return (
+    <div>
+      <div>{"text value: " + text}</div>
+      <div>{"debounceText value: " + debounceText}</div>
+      <div>
+        <input
+          value={text}
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+        ></input>
+      </div>
+    </div>
+  );
 }
 
 export default DebounceWithUseEffect;

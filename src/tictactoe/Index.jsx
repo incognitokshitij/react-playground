e771accf4fd4acee0863b846f12fd74eb39523f1
incognitokshitij <kshitijvarshney92@gmail.com>
2026-06-23 21:37:03 +0530

    init tic tac toe

--- src/tictactoe/Index.jsx
+++ src/tictactoe/Index.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 function Index() {
-  return <div>Index</div>;
+  const size = 3;
+    const { board, getMessage, resetGame, fill } = useTicTacToe(size);
+    return (
+        <div className='container'>
+            <div>
+                <div>
+                    <div className='header'>Tic Tac Toe Game</div>
+                    <div className='player-div'>
+                        <div>{getMessage()}</div>
+                        <button className='reset-btn' onClick={resetGame}>Reset Game</button>
+                    </div>
+
+                </div>
+                <div className='board' style={{ gridTemplateColumns: `repeat(${size}, auto)` }}>
+                    {board.map((cell, index) => {
+                        return <div onClick={() => { fill(index) }} className={"cell" + (cell ? " disabledCell" : "")}>{cell}</div>
+                    })}</div>
+            </div>
+        </div>
 }
 
 export default Index;

27b0d332913bc020957dbcbdc9e73efdda68016b
incognitokshitij <kshitijvarshney92@gmail.com>
2026-06-23 21:52:37 +0530

    tic tac toe done

--- src/tictactoe/Index.jsx
+++ src/tictactoe/Index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./style.css";
+import useTicTacToe from "./useTicTacToe";
 
 function Index() {
   const size = 3;
--- src/tictactoe/useTicTacToe.jsx
+++ src/tictactoe/useTicTacToe.jsx
@@ -1,11 +1,73 @@
-import React from "react";
+import {useState} from "react";
 
-function useTicTacToe() {
+function useTicTacToe(size) {
   const [board, setBoard] = useState(Array(size * size).fill(null));
   const [player, setPlayer] = useState("X");
   const [isWin, setIsWin] = useState(false);
 
-  return <div>useTicTacToe</div>;
+  function getMessage() {
+    if (isWin) {
+      return "Player " + (player === "X" ? "O" : "X") + " win";
+    }
+    let anyEmpty = board.some((cell) => !cell);
+    if (!anyEmpty) {
+      return "Match Draw";
+    }
+
+    return "Player " + player + " turn";
+  }
+
+  function resetGame() {
+    setBoard(Array(size * size).fill(null));
+    setPlayer("X");
+    setIsWin(false);
+  }
+  function anyWin(currentBoard, playerTurn) {
+    // diagonal
+    let d1 = 0,
+      d2 = 0;
+    for (let i = 0; i < size; i++) {
+      if (currentBoard[i * (size + 1)] === playerTurn) d1++;
+      if (currentBoard[(i + 1) * (size - 1)] === playerTurn) d2++;
+    }
+    if (d1 === size || d2 === size) return true;
+
+    // row
+    for (let i = 0; i < size; i++) {
+      let count = 0;
+      for (let j = 0; j < size; j++) {
+        if (currentBoard[i * size + j] === playerTurn) count++;
+      }
+      if (count === size) return true;
+    }
+
+    // col
+    for (let i = 0; i < size; i++) {
+      let count = 0;
+      for (let j = 0; j < size; j++) {
+        if (currentBoard[j * size + i] === playerTurn) count++;
+      }
+      if (count === size) return true;
+    }
+
+    return false;
+  }
+  console.log(player);
+
+  function fill(index) {
+    if (board[index] || isWin) return;
+    const newBoard = [...board];
+    newBoard[index] = player;
+    setBoard(newBoard);
+    let win = anyWin(newBoard, player);
+    setPlayer((prev) => (prev == "X" ? "O" : "X"));
+    if (win) {
+      setIsWin(true);
+      return;
+    }
+  }
+
+  return { board, getMessage, resetGame, fill };
 }
 
 export default useTicTacToe;

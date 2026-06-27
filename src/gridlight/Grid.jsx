345a8701d360a20e84d8999ad432e0349d791613
incognitokshitij <kshitijvarshney92@gmail.com>
2026-06-27 15:26:24 +0530

    seting up grid

--- src/gridlight/Grid.jsx
+++ src/gridlight/Grid.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
 function GridBox() {
+  const { row, column } = props;
+  const [matrix, setMatrix] = useState([]);
+  const [isDragging, setIsDragging] = useState(false);
+  const [startCell, setStartCell] = useState(null);
+  const [endCell, setEndCell] = useState(null);
   return <div></div>;
 }
 

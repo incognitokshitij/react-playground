3e84dae7d317d5fe28aa0c71f0eba64535462a5f
incognitokshitij <kshitijvarshney92@gmail.com>
2026-06-28 17:25:55 +0530

    row and column configuration

--- src/gridlight/Grid.jsx
+++ src/gridlight/Grid.jsx
@@ -7,6 +7,17 @@ function GridBox() {
   const [isDragging, setIsDragging] = useState(false);
   const [startCell, setStartCell] = useState(null);
   const [endCell, setEndCell] = useState(null);
+
+   useEffect(() => {
+        let count = 1;
+        const newMatrix = Array.from({ length: row }, (_, i) =>
+            Array.from({ length: column }, (_, j) => ({
+                position: { i, j },
+                index: count++
+            }))
+        );
+        setMatrix(newMatrix);
+    }, [row, column]);
   return <div></div>;
 }
 

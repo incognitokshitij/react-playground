7b02e7febb44d489ce86ac0aae766eab206d0c61
incognitokshitij <kshitijvarshney92@gmail.com>
2026-07-02 22:18:41 +0530

    optimization

--- src/nestedCheckbox/Index.jsx
+++ src/nestedCheckbox/Index.jsx
@@ -1,28 +1,30 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { Link, Routes, Route } from "react-router-dom";
 import "./style.css";
-import Recursive from "./recursive/Recursive";
-import Optimized from "./optimised/Optimized";
+
+const Recursive = lazy(() => import("./recursive/Recursive"));
+const Optimized = lazy(() => import("./optimised/Optimized"));
+
+const Home = () => (
+  <div className="container">
+    <div className="container-view">
+      <div className="heading">Nested Checkbox</div>
+      <Link to="/nested-checkbox/recursive">Recursive</Link>
+      <Link to="/nested-checkbox/optimized">Optimized</Link>
+    </div>
+  </div>
+);
 
 function Index() {
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <div className="container">
-            <div className="container-view">
-              <div className="heading">Nested Checkbox</div>
-              <Link to="/nested-checkbox/recursive">Recursive</Link>
-              <Link to="/nested-checkbox/optimized">Optimized</Link>
-            </div>
-          </div>
-        }
-      />
-      <Route path="/recursive" element={<Recursive />} />
-      <Route path="/optimized" element={<Optimized />} />
-    </Routes>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/recursive" element={<Recursive />} />
+        <Route path="/optimized" element={<Optimized />} />
+      </Routes>
+    </Suspense>
   );
 }
 
-export default Index;
+export default React.memo(Index);
